743d5abd7e7b85c03d6e024af6530043938a729b
parhawm112 <117488273+parhawm112@users.noreply.github.com>
2023-01-14 12:14:16 -0800

    Add files via upload

---
+++ p52.main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int character;
+ cout <<"Please enter character :";
+ cin >>character;
+ 
+ switch(character){
+ 	case 'a':
+ 		cout <<"VOWEL";
+ 		break;
+ 	case 'e':
+ 		cout <<"VOWEL";
+ 		break;
+ 	case 'i':
+ 		cout <<"VOWEL";
+ 		break;
+ 	case 'o':
+ 		cout <<"VOWEL";
+ 		break;
+ 	case 'u':
+ 		cout <<"VOWEL";
+ 		break;
+ 	case 'y':
+ 		cout <<"VOWEL";
+ }
+  
+}
